3bf77054c1dd3b728f3343d238d914d6d0081534
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-09 23:10:33 -0500

    Feat: Importe la función de conexión en el index para iniciar nuestra base de datos Mongo

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
 const express = require('express');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
+const connectDB = require('./database');
 
 const { port, dbUrl, secret } = config;
 const app = express();
 
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
+connectDB(dbUrl); // llamamos a la función que conecta con nuestra base de datos en Mongo DB Atlas
 
-app.set('config', config);
+app.set('config', config); // añadimos propiedades a app.
 app.set('pkg', pkg);
 
-// parse application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded --middleware.
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(authMiddleware(secret));
@@ -25,7 +30,6 @@ routes(app, (err) => {
   }
 
   app.use(errorHandler);
-
   app.listen(port, () => {
     console.info(`App listening on port ${port}`);
   });
