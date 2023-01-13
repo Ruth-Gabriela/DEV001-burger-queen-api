5a676a1005375b7120b7dc97bacf1657c8fdf67a
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-13 02:26:02 -0500

    Feat: Modifiqué controlador Users | Agregué función getUsers

--- controller/users.js
+++ controller/users.js
@@ -1,8 +1,17 @@
+const User = require('../models/User');
+
 module.exports = {
-  getUsers: (req, res, next) => {
-    res.send('hola');
+  getUsers: async (req, res, next) => {
+    // método find() de mongoose devuelve toda la data de una collection.
+    try {
+      const users = await User.find(); // id prueba error { _id: '63be4f99954170b25e100f7e' }
+      if (users.length > 0) {
+        res.status(200).send({ users });
+      } else {
+        res.status(404).send({ message: 'no hay usuarios en la DB' });
+      }
+    } catch (error) {
+      res.status(500).send({ message: error.message });
+    }
   },
-  /* getUserId: (req, res, next) =>{
-    res.send("mostrar id del usuario");
-  }, */
 };
