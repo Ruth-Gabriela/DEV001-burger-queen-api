5bde0f6afca2a1165b48a3861d1f14bd5d59e477
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-17 22:02:40 -0500

    Feat: Agregué la lógica para la autentificación de usuario | closes #14

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
+const User = require('../models/User');
 
 const { secret } = config;
 
-/** @module auth */
+/** @module auth */ // => login
 module.exports = (app, nextMain) => {
   /**
    * @name /auth
@@ -17,15 +18,25 @@ module.exports = (app, nextMain) => {
    * @code {400} si no se proveen `email` o `password` o ninguno de los dos
    * @auth No requiere autenticación
    */
-  app.post('/auth', (req, resp, next) => {
+  app.post('/auth', async (req, res, next) => {
     const { email, password } = req.body;
-
     if (!email || !password) {
       return next(400);
     }
-
     // TODO: autenticar a la usuarix
-    next();
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send({ error: 'No existe el usuario' });
+      }
+      const comparePassword = await user.comparePassword(password);
+      if (!comparePassword) {
+        return res.status(403).send({ error: 'Contraseña Incorrecta' });
+      }
+      return res.status(200).send({ user });
+    } catch (error) {
+      return res.status(500).send({ error: error.message });
+    }
   });
 
   return nextMain();
