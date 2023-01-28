ee1273958e902651a13428cb1807efaaad8a6d50
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-28 02:52:58 -0500

    Fix: Agregar handler de error a Connect de mongodb

--- database.js
+++ database.js
@@ -4,9 +4,8 @@ const connectDB = async () => {
   try {
     console.info();
     mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.DB_URL, () => {
-      console.info('Connected to MongoDB');
-    });
+    await mongoose.connect(process.env.DB_URL).then(() => console.info('Connected with MongoDB'))
+      .catch((e) => console.info(e));
   } catch (error) {
     console.info("Can't Connect with MongoDB");
   }
