3cb1cc00daa85673a9d34cf5c99456f8e713947b
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-28 16:06:59 -0500

    Preparing for Deploy Backend

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-exports.port = process.argv[2] || process.env.PORT || 8888;
-exports.dbUrl = process.env.MONGO_URL || process.env.DB_URL || 'mongodb://localhost:27017/test';
-exports.secret = process.env.JWT_SECRET || 'esta-es-la-api-burger-queen';
-exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost.com';
-exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
-exports.dbName = process.env.DB_NAME || 'burgerqueen';
+exports.port = process.env.PORT;
+exports.dbUrl = process.env.DB_URL;
+exports.secret = process.env.JWT_SECRET;
+exports.adminEmail = process.env.ADMIN_EMAIL;
+exports.adminPassword = process.env.ADMIN_PASSWORD;
+exports.dbName = process.env.DB_NAME;
