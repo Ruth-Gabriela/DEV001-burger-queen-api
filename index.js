7de983f4a5c47665dcec75c717c7b17b213c6062
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-30 16:47:37 -0500

    Agregué dependencia CORS para errores de acceso

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
+const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config');
@@ -20,11 +21,11 @@ app.set('config', config); // añadimos propiedades a app.
 app.set('pkg', pkg);
 
 // parse application/x-www-form-urlencoded --middleware.
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
