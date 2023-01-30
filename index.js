7179e60aa4fc38060cb22d943e4f0fd4855f3fe0
gabyzam <ruth.gabriela.pz@gmail.com>
2023-01-30 16:51:52 -0500

    Fix CORS

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
 
 dotenv.config();
-const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config');
@@ -21,11 +20,6 @@ app.set('config', config); // añadimos propiedades a app.
 app.set('pkg', pkg);
 
 // parse application/x-www-form-urlencoded --middleware.
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
